c87aa0f1975832a1c60912d6ab459cfc04cdd7f6
Audrey Zhao <audreyzhao@Audreys-MBP.lan>
2022-07-27 23:56:43 -0700

    fixed links

--- JS/AllCases.js
+++ JS/AllCases.js
@@ -12,11 +12,30 @@ async function init() {
 
     // Make and append the cards
     const container = document.querySelector("#cases-container");
+
+    const sectionTitles = [];
+
     for (let i = 0; i < data.cards.length; i++) {
         let card = document.createElement("case-card");
         card.data = data.cards[i];
-        container.appendChild(card);
-        console.log(data.cards[i]);
+        // find which section to put it in
+        if(sectionTitles.includes(data.cards[i].year)) {
+            const yearElement = document.getElementById(data.cards[i].year);
+            yearElement.parentNode.insertBefore(card, yearElement.nextSibling);
+            // yearElement.appendChild(card);
+        }
+        else {
+            const newTitle = document.createElement("h1");
+            newTitle.className = "section-title";
+            newTitle.setAttribute("id", data.cards[i].year);
+            newTitle.innerHTML = data.cards[i].year;
+            sectionTitles.push(data.cards[i].year);
+            container.appendChild(newTitle);
+            container.appendChild(card);
+        }
+
+        // container.appendChild(card);
+        // console.log(data.cards[i]);
     }
 
     // // console.log(recipeData);
